1f24e580d49ecad6f29e1cb47c1fb0600509e684
m.schlueter01 <m.schlueter01@stud.uni-goettingen.de>
2020-04-02 17:53:12 +0200

    first running client socket

--- include/App.h
+++ include/App.h
@@ -1,8 +1,22 @@
 #ifndef _APP_H_
 #define _APP_H_
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-typedef struct{
-	int code;
+typedef struct
+{
+	int sockfd;
+	struct hostent *hostent;
+	struct sockaddr_in adress;
 } app_data;
 
 int app_initialize(app_data **data);
--- src/App.c
+++ src/App.c
@@ -1,10 +1,39 @@
 #include "App.h"
-#include <stdio.h>
-
 
 int app_initialize(app_data **data)
 {
 	printf("Initialize app\n");
+	*data = (app_data *)malloc(sizeof(app_data));
+
+	(*data)->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if ((*data)->sockfd < 0)
+		return -1;
+
+	(*data)->hostent = gethostbyname("rkp.intecelektro.de");
+
+	memset(&((*data)->adress), 0, sizeof((*data)->adress));
+	(*data)->adress.sin_family = AF_INET;
+	//(*data)->adress.sin_addr.s_addr = inet_addr((*data)->hostent->h_addr_list[0]);
+	(*data)->adress.sin_addr.s_addr = inet_addr("10.250.0.10");
+	(*data)->adress.sin_port = htons(30000);
+
+	if (connect((*data)->sockfd, (struct sockaddr*)&(*data)->adress, sizeof((*data)->adress)) < 0)
+		printf("ERROR connecting\n");
+
+	const char get_request_msg[] = "GET /api/user/10 HTTP/1.1\r\n\r\n";
+	write((*data)->sockfd,get_request_msg,strlen(get_request_msg));
+
+	char content[1000];
+	memset(content,0,sizeof(content));
+
+	read((*data)->sockfd,content,sizeof(content));
+
+	close((*data)->sockfd);
+
+	content[sizeof(content)-1] = 0;
+	printf(content);
+	printf("\n");
+
 	return 0;
 }
 int app_run(app_data *data)
